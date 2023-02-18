aae889d3cef267c4a7694efe063397d63c6b8a53
Shota-Kurahashi <Myheroes0715@gmail.com>
2023-02-18 17:18:37 +0900

    change vercel

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@
 /* eslint-disable consistent-return */
 /* eslint-disable import/no-extraneous-dependencies */
 import * as dotenv from "dotenv";
-import express, { Response } from "express";
+import express from "express";
 import { scheduleJob } from "node-schedule";
 import { createAllEpisodes } from "src/createAllEpisodes";
 import { updateEpisodes } from "src/hooks";
@@ -21,9 +21,16 @@ scheduleJob("46 20 * * * *", async () => {
 });
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (_, res: Response) => {
-  res.send("Hello World!");
+app.get("/", (req: any, res: any) => {
+  try {
+    res.send({ name: "hoge" });
+  } catch (error) {
+    res.sendStatus(500);
+  }
 });
 
 app.listen(process.env.PORT || 3000);
+export default app;
