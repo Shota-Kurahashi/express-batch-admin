444e19ae2b6d37e595c14a8af0b674227cce8250
Shota-Kurahashi <Myheroes0715@gmail.com>
2023-02-18 16:59:39 +0900

    add express

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@
 /* eslint-disable consistent-return */
 /* eslint-disable import/no-extraneous-dependencies */
 import * as dotenv from "dotenv";
+import express, { Response } from "express";
 import { scheduleJob } from "node-schedule";
 import { createAllEpisodes } from "src/createAllEpisodes";
 import { updateEpisodes } from "src/hooks";
@@ -19,4 +20,10 @@ scheduleJob("46 20 * * * *", async () => {
   }
 });
 
-console.log("OK");
+const app = express();
+
+app.get("/", (_, res: Response) => {
+  res.send("Hello World!");
+});
+
+app.listen({ port: process.env.PORT || 3005 });
