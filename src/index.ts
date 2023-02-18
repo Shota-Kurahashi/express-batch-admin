f7372d6c9e0edd146478aac02eee766d73798e3d
Shota-Kurahashi <Myheroes0715@gmail.com>
2023-02-18 17:28:36 +0900

    git workflow

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,18 @@
 /* eslint-disable consistent-return */
 /* eslint-disable import/no-extraneous-dependencies */
 import * as dotenv from "dotenv";
+import { createAllEpisodes } from "src/createAllEpisodes";
+import { updateEpisodes } from "src/hooks";
 
 dotenv.config();
 
 (async () => {
-  // try {
-  //   await createAllEpisodes();
-  //   await updateEpisodes();
-  //   console.log("success");
-  // } catch {
-  //   console.error("error");
-  //   throw new Error("error");
-  // }
-  console.log("success");
+  try {
+    await createAllEpisodes();
+    await updateEpisodes();
+    console.log("success");
+  } catch {
+    console.error("error");
+    throw new Error("error");
+  }
 })();
